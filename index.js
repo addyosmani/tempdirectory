c68d4220031718f4153c845c4755391d79ff53d5
Addy Osmani <addyosmani@gmail.com>
2016-12-21 13:07:47 -0800

    update implementation to just use dir

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
 var path = require('path');
 var osTmpdir = require('os-tmpdir');
 var uuid = require('uuid');
+
 var TMP_DIR = osTmpdir();
 
-module.exports = function (ext) {
-	return path.join(TMP_DIR, uuid.v4() + (ext || ''));
+module.exports = function () {
+	return path.join(TMP_DIR, uuid.v4());
 };
